18afd2d969a4e70ddfa89d72d4ee1aad3cdbf796
Max Chadwick <mpchadwick@gmail.com>
2019-03-30 15:32:41 -0400

    Scaffold anonymizer

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import "bufio"
 import "fmt"
+import "github.com/mpchadwick/dbanon/src"
 import "os"
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, err := reader.ReadString('\n')
-		fmt.Print(text)
+		fmt.Print(anomymizer.Anonymize(text))
 
 		if err != nil {
 			break
---
+++ src/anonymizer.go
@@ -0,0 +1,5 @@
+package anomymizer
+
+func Anonymize(s string) string {
+	return s
+}
