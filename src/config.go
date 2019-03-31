aeb3cdcc25f18c2637f2918b7fb99f1ec394f495
Max Chadwick <mpchadwick@gmail.com>
2019-03-31 11:13:16 -0400

    Pull config from go-bindata

--- src/config.go
+++ src/config.go
@@ -1,7 +1,6 @@
 package dbanon
 
 import "gopkg.in/yaml.v2"
-import "io/ioutil"
 
 type Config struct {
 	Tables []string
@@ -10,8 +9,7 @@ type Config struct {
 func NewConfig() *Config {
 	c := &Config{}
 
-	// TODO - Properly bundle this
-	source, _ := ioutil.ReadFile("/Users/maxchadwick/go/src/github.com/mpchadwick/dbanon/etc/magento2.yml")
+	source, _ := Asset("etc/magento2.yml")
 	yaml.Unmarshal(source, &c)
 
 	return c
