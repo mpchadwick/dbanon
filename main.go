69a2c15d47951d1278b749063c2e3ccb464fbe20
Max Chadwick <mpchadwick@gmail.com>
2019-03-30 21:08:38 -0400

    Add working parsing

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "bufio"
 import "fmt"
 import "github.com/mpchadwick/dbanon/src"
+import "io/ioutil"
+import "log"
 import "os"
 
 func main() {
+	// sqlparser can be noisy
+	// https://github.com/xwb1989/sqlparser/blob/120387863bf27d04bc07db8015110a6e96d0146c/ast.go#L52
+	// We don't want to hear about it
+	log.SetOutput(ioutil.Discard)
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, err := reader.ReadString('\n')
--- src/anonymizer.go
+++ src/anonymizer.go
@@ -1,5 +1,14 @@
 package anomymizer
 
+import "github.com/xwb1989/sqlparser"
+
 func Anonymize(s string) string {
-	return s
+	stmt, _ := sqlparser.Parse(s)
+
+	switch stmt := stmt.(type) {
+	case *sqlparser.Insert:
+		return stmt.Table.Name.String() + "\n"
+	default:
+		return ""
+	}
 }
