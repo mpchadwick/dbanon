09bbe0c2f7d6f0bf256f52b6a7fa769d8c871b5f
Max Chadwick <mpchadwick@gmail.com>
2019-04-02 21:10:48 -0400

    Add tests for ProcessColumn

--- src/config_test.go
+++ src/config_test.go
@@ -8,10 +8,32 @@ func TestProcessTable(t *testing.T) {
 	c := NewConfig()
 
 	if !c.ProcessTable("admin_user") {
-		t.Errorf("Expected %t", true)
+		t.Error("Got false want true")
 	}
 
 	if c.ProcessTable("catalog_product") {
-		t.Errorf("Expected %t", false)
+		t.Error("Got true want false")
 	}
+}
+
+func TestProcessColumn(t *testing.T) {
+	c := NewConfig()
+
+	process, format := c.ProcessColumn("admin_user", "firstname")
+	if !process {
+		t.Error("Got false want true")
+	}
+	if format != "firstname" {
+		t.Errorf("Got %s want firstname", format)
+	}
+
+	process2, format2 := c.ProcessColumn("foo", "bar")
+	if process2 {
+		t.Error("Got true want false")
+	}
+	if format2 != "" {
+		t.Errorf("Got %s want empty string", format)
+	}
+
+
 }
