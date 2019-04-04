c4cb4151d44f21562f856f029de5e4a7b1d1e031
Max Chadwick <mpchadwick@gmail.com>
2019-04-03 22:23:49 -0400

    Continue building out M2 anonymization

--- src/provider.go
+++ src/provider.go
@@ -28,6 +28,24 @@ func (p Provider) Get(s string) string {
 		return faker.Internet().UserName()
 	case "password":
 		return faker.Internet().Password(8, 14)
+	case "datetime":
+		return faker.Date().Birthday(0, 40).Format("2006-01-02 15:04:05")
+	case "customer_suffix":
+		return faker.Name().Suffix()
+	case "ipv4":
+		return faker.Internet().IpV4Address()
+	case "state":
+		return faker.Address().State()
+	case "postcode":
+		return faker.Address().Postcode()
+	case "street":
+		return faker.Address().StreetAddress()
+	case "telephone":
+		return faker.PhoneNumber().PhoneNumber()
+	case "title":
+		return faker.Name().Prefix()
+	case "company":
+		return faker.Company().Name()
 	}
 
 	return ""
