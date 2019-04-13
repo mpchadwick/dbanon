0a6db3d2d76cddbfea2b552e7c9741392d534d48
Max Chadwick <mpchadwick@gmail.com>
2019-04-12 21:33:34 -0400

    Use correct entity_type_code

--- src/eav_test.go
+++ src/eav_test.go
@@ -7,7 +7,7 @@ import (
 func TestEavProcessLine(t *testing.T) {
 	config, _ := NewConfig("magento2")
 	eav := NewEav(config)
-	eav.ProcessLine("INSERT INTO `eav_entity_type` (`entity_type_id`, `entity_type_code`) VALUES (1, 'customer_entity');")
+	eav.ProcessLine("INSERT INTO `eav_entity_type` (`entity_type_id`, `entity_type_code`) VALUES (1, 'customer');")
 	eav.ProcessLine("INSERT INTO `eav_attribute` (`attribute_id`, `entity_type_id`, `attribute_code`) VALUES (1, 1, 'firstname');")
 	r1 := false
 	for _, eavConfig := range eav.Config.Eav {
