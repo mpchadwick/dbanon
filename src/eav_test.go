b722d9b58ab869ba0cbfdc0019e199d459929eff
Max Chadwick <mpchadwick@gmail.com>
2019-04-12 21:22:20 -0400

    Add test for EAV mapping

---
+++ src/eav_test.go
@@ -0,0 +1,25 @@
+package dbanon
+
+import (
+	"testing"
+)
+
+func TestEavProcessLine(t *testing.T) {
+	config, _ := NewConfig("magento2")
+	eav := NewEav(config)
+	eav.ProcessLine("INSERT INTO `eav_entity_type` (`entity_type_id`, `entity_type_code`) VALUES (1, 'customer_entity');")
+	eav.ProcessLine("INSERT INTO `eav_attribute` (`attribute_id`, `entity_type_id`, `attribute_code`) VALUES (1, 1, 'firstname');")
+	r1 := false
+	for _, eavConfig := range eav.Config.Eav {
+		for k, v := range eavConfig.Attributes {
+			if k == "1" && v == "firstname" {
+				r1 = true
+			}
+		}
+	}
+
+	if !r1 {
+		t.Errorf("Got false wanted true")
+	}			
+
+}
