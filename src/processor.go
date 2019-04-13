920ff0de43429c5f6d5d9f677afd7a91ddd6b4bf
Max Chadwick <mpchadwick@gmail.com>
2019-04-13 16:19:41 -0400

    Remove uncessary type assertion

--- src/processor.go
+++ src/processor.go
@@ -22,59 +22,54 @@ func (p LineProcessor) ProcessLine(s string) string {
 	}
 
 	stmt, _ := sqlparser.Parse(s)
+	insert := stmt.(*sqlparser.Insert)
 
-	switch stmt := stmt.(type) {
-	case *sqlparser.Insert:
+	table := insert.Table.Name.String()
 
-		table := stmt.Table.Name.String()
+	processor := p.Config.ProcessTable(table)
 
-		processor := p.Config.ProcessTable(table)
+	switch processor {
+	case "":
+		// This table doesn't need to be processed
+		return s
+	case "table":
+		// "Classic" processing
+		rows := insert.Rows.(sqlparser.Values)
+		for _, vt := range rows {
+			for i, e := range vt {
+				column := insert.Columns[i].String()
 
-		switch processor {
-		case "":
-			// This table doesn't need to be processed
-			return s
-		case "table":
-			// "Classic" processing
-			rows := stmt.Rows.(sqlparser.Values)
-			for _, vt := range rows {
-				for i, e := range vt {
-					column := stmt.Columns[i].String()
+				result, dataType := p.Config.ProcessColumn(table, column)
 
-					result, dataType := p.Config.ProcessColumn(table, column)
-
-					if !result {
-						continue
-					}
-
-					e.(*sqlparser.SQLVal).Val = []byte(p.Provider.Get(dataType))
-
+				if !result {
+					continue
 				}
+
+				e.(*sqlparser.SQLVal).Val = []byte(p.Provider.Get(dataType))
+
 			}
-			return sqlparser.String(stmt) + ";\n"
-		case "eav":
-			// EAV processing
-			var attributeId string
-			rows := stmt.Rows.(sqlparser.Values)
-			for _, vt := range rows {
-				for i, e := range vt {
-					column := stmt.Columns[i].String()
-					if column == "attribute_id" {
-						switch v := e.(type) {
-						case *sqlparser.SQLVal:
-							switch v.Type {
-							default:
-								attributeId = string(v.Val)
-							}
+		}
+		return sqlparser.String(insert) + ";\n"
+	case "eav":
+		// EAV processing
+		var attributeId string
+		rows := insert.Rows.(sqlparser.Values)
+		for _, vt := range rows {
+			for i, e := range vt {
+				column := insert.Columns[i].String()
+				if column == "attribute_id" {
+					switch v := e.(type) {
+					case *sqlparser.SQLVal:
+						switch v.Type {
+						default:
+							attributeId = string(v.Val)
 						}
 					}
 				}
 			}
-
-			return "FOOO" + attributeId + "\n"
-		default:
-			return s
 		}
+
+		return "FOOO" + attributeId + "\n"
 	default:
 		return s
 	}
