1e234aa50b5f36fcd1463be43722586075abad14
Max Chadwick <mpchadwick@gmail.com>
2020-11-03 22:00:23 -0500

    Remove one level of indentation from EAV insert processing

--- src/eav.go
+++ src/eav.go
@@ -30,49 +30,48 @@ func (eav Eav) ProcessLine(s string) {
 
 func (eav Eav) processInsert (s string) {
 	stmt, _ := sqlparser.Parse(s)
-	switch stmt := stmt.(type) {
-	case *sqlparser.Insert:
-		table := stmt.Table.Name.String()
-		if table == "eav_entity_type" {
-			var entityTypeId string
-			rows := stmt.Rows.(sqlparser.Values)
-			for _, vt := range rows {
-				for i, e := range vt {
-					column := currentTable[i]
-					switch v := e.(type) {
-					case *sqlparser.SQLVal:
-						if column == "entity_type_id" {
-							entityTypeId = string(v.Val)
-						}
-						if column == "entity_type_code" {
-							eav.entityMap[string(v.Val)] = entityTypeId
-						}
+	insert := stmt.(*sqlparser.Insert)
+
+	table := insert.Table.Name.String()
+	if table == "eav_entity_type" {
+		var entityTypeId string
+		rows := insert.Rows.(sqlparser.Values)
+		for _, vt := range rows {
+			for i, e := range vt {
+				column := currentTable[i]
+				switch v := e.(type) {
+				case *sqlparser.SQLVal:
+					if column == "entity_type_id" {
+						entityTypeId = string(v.Val)
+					}
+					if column == "entity_type_code" {
+						eav.entityMap[string(v.Val)] = entityTypeId
 					}
 				}
 			}
 		}
-		if table == "eav_attribute" {
-			var attributeId string
-			var entityTypeId string
-			rows := stmt.Rows.(sqlparser.Values)
-			for _, vt := range rows {
-				for i, e := range vt {
-					column := currentTable[i]
-					switch v := e.(type) {
-					case *sqlparser.SQLVal:
-						if column == "attribute_id" {
-							attributeId = string(v.Val)
-						}
-						if column == "entity_type_id" {
-							entityTypeId = string(v.Val)
-						}
-						if column == "attribute_code" {
-							for _, eavConfig := range eav.Config.Eav {
-								if eav.entityMap[eavConfig.Name] == entityTypeId {
-									for eavK, eavV := range eavConfig.Attributes {
-										if eavK == string(v.Val) {
-											eavConfig.Attributes[attributeId] = eavV
-										}
+	}
+	if table == "eav_attribute" {
+		var attributeId string
+		var entityTypeId string
+		rows := insert.Rows.(sqlparser.Values)
+		for _, vt := range rows {
+			for i, e := range vt {
+				column := currentTable[i]
+				switch v := e.(type) {
+				case *sqlparser.SQLVal:
+					if column == "attribute_id" {
+						attributeId = string(v.Val)
+					}
+					if column == "entity_type_id" {
+						entityTypeId = string(v.Val)
+					}
+					if column == "attribute_code" {
+						for _, eavConfig := range eav.Config.Eav {
+							if eav.entityMap[eavConfig.Name] == entityTypeId {
+								for eavK, eavV := range eavConfig.Attributes {
+									if eavK == string(v.Val) {
+										eavConfig.Attributes[attributeId] = eavV
 									}
 								}
 							}
