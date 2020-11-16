48ff6b2aac9f6d62f5b364a4ebd2e151baa11c7d
Max Chadwick <mpchadwick@gmail.com>
2020-11-15 21:59:59 -0500

    Add comments to logger

--- src/logger.go
+++ src/logger.go
@@ -10,10 +10,15 @@ func init() {
 	logger = logrus.New()
 }
 
+// SetLogger sets the logger instance
+// This is useful in testing as the logger can be overridden
+// with a test logger
 func SetLogger(l *logrus.Logger) {
 	logger = l
 }
 
+// GetLogger returns the logger instance.
+// This instance is the entry point for all logging
 func GetLogger() *logrus.Logger {
 	return logger
 }
